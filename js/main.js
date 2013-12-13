2e7a062e805b71ed1fea8c1a181691fe3ac54345
MakerSquare Student <students@themakersquare.com>
2013-12-13 11:16:21 -0600

    Add passing code through exercise #7

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 alert('hi');
+
+var itemTemplate = $('#templates .item').html();
+
+var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+$('#store').append(newItemHtml);
+
+var items = [
+
+  { name: 'Grapes', price: 1.99 },
+  { name: 'Strawberries', price: 3.99 },
+  { name: 'Kiwi', price: 2.99 }
+];
+for (var i=0;i<items.length;i++)
+{
+$('#store').append($.render(itemTemplate, {name: items[i].name, price: items[i].price}))
+
+};
+
