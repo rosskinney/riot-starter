8b8711118b1990afff4a2d0e4c48862043a2a96d
MakerSquare Student <students@themakersquare.com>
2013-12-14 00:56:19 -0600

    Add working code for riot_templates_ex

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-alert('hi');
+
+
+var itemTemplate = $('#templates .item').html();
 
 var newItemHtml = $.render(itemTemplate, {
   name: 'Dewberry',
   price: 0.15
+});
+
+$("#submit").on("click", function(e) {
+	e.preventDefault();
+	var newStoreHtml = $.render(itemTemplate, {name:$("#name").val(), price:$("#price").val()} );
+	$("#store").append(newStoreHtml);
 });
